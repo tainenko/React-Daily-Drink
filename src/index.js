5b9c2a6113eebe072484562f72b18041b6517cdf
ko tainen <TonyK@hyvesolutions.com>
2020-03-21 00:33:02 +0800

    add Provider in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import Form from "./js/components/Form";
+import Provider from "react-redux";
+import store from "./components/store";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const wrapper = document.getElementById("container");
+wrapper ? ReactDOM.render(
+    <Provider store={store}>
+
+    </Provider>
+    , wrapper) :false;
