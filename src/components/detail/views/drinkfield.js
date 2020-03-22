7e62c7928de3fe902bfb6318ba65fa2cd1d90987
ko tainen <TonyK@hyvesolutions.com>
2020-03-22 15:19:06 +0800

    update drinkfiled with CustomTag

--- src/components/detail/views/drinkfield.js
+++ src/components/detail/views/drinkfield.js
@@ -2,13 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import {useField} from "formik";
 
-const DrinkField = ({label,...props}) => {
-    const [field,meta,helpers]=useField(props)
+const DrinkField = ({label, as = "input", ...props}) => {
+    const [field, meta, helpers] = useField(props);
+    const CustomTag = `${as}`;
     return (
         <div className="m-b-10">
             <label>
                 {label}
-                <input {...field} {...props} />
+                <CustomTag {...field} {...props} />
             </label>
             {meta.error ? (
                 <div className='error'>{meta.error}</div>
@@ -19,6 +20,7 @@ const DrinkField = ({label,...props}) => {
 
 DrinkField.propTypes = {
     label: PropTypes.string.isRequired,
+    as: PropTypes.string
 };
 
 
