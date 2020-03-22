ae464768268c5351b71acc7afad392781066c887
ko tainen <TonyK@hyvesolutions.com>
2020-03-22 12:47:56 +0800

    update isRequired in header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,13 @@
 import React from "react";
-const Header=({title})=>(
+import Proptypes from "prop-types";
+
+const Header = ({title}) => (
     <div className="dailyDrink__title">
         <p className="title p-20">title</p>
     </div>
 );
+Header.proptypes = {
+    title: Proptypes.string.isRequired
+};
 
 export default Header;
