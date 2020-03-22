165a77a403cbbc3cf377d10f6dd67aa05559a37d
ko tainen <TonyK@hyvesolutions.com>
2020-03-22 18:38:55 +0800

    update orderform

--- src/components/detail/views/orderform.js
+++ src/components/detail/views/orderform.js
@@ -1,51 +1,22 @@
 import React from 'react';
-import {connect} from "react-redux"
 import {AddOrder} from '../actions'
-import {Formik, Field, Form} from "formik";
+import {Formik} from "formik";
 import OrderSchema from "./orderschema";
-import ErrorInject from "./errorinject";
-import ErrorMessage from "./errormessage";
-import DrinkField from "./drinkfield";
 import "./style.scss"
 import "../../drinklist/views/style.scss"
+import InnerForm from "./innerForm";
 
 
-const OrderForm = ({AddOrder}) => (
-    <div>
-        <div className="dailyDrink__title">
-            <p className="title p-20">Drink Order</p>
-        </div>
-
-        <Formik
-            initialValues={{}}
-            validationSchema={OrderSchema}
-            onSubmit={(values, actions) => {
-                alert(JSON.stringify(values, null, 2));
-                actions.setSubmitting(false);
-            }}>
-            {({
-                  values,
-                  errors,
-                  touched,
-                  handleChange,
-                  handleBlur,
-                  handleSubmit,
-                  isSubmitting,
-                  /* and other goodies */
-              }) => (
-                <Form className="dailyDrink__orderList">
-                    <ErrorInject errors={errors}>
-                        <DrinkField label="Name:" name="name" placeholder="請輸入飲料"/>
-                        <DrinkField label="Price:" name="price" filedtype="number" placeholder="請輸入價錢"/>
-                        <DrinkField label="Notes:" name="notes" as="textarea" placeholder="請輸入飲料"/>
-                    </ErrorInject>
-                    <div className="dailyDrink__orderList__control">
-                        <button type="submit" className="btn btn-add transition">Submit</button>
-                        <button className="btn btn-delete transition">Cancel</button>
-                    </div>
-                </Form>)}
-        </Formik>
-    </div>
+const OrderForm = ({initialValues = {}}) => (
+    <Formik
+        initialValues={initialValues}
+        validationSchema={OrderSchema}
+        onSubmit={(values, actions) => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+        }}>
+        <InnerForm/>
+    </Formik>
 );
 
 const getOrderById = (state, id) => {
@@ -65,4 +36,4 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 
-export default connect(null, mapDispatchToProps)(OrderForm);
+export default OrderForm;
