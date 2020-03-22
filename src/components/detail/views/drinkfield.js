88f944f66c0e6568fc1423fc6341f3b906fef687
ko tainen <TonyK@hyvesolutions.com>
2020-03-22 11:30:58 +0800

    add fieldtype

--- src/components/detail/views/drinkfield.js
+++ src/components/detail/views/drinkfield.js
@@ -3,19 +3,20 @@ import PropTypes from "prop-types";
 import {Field} from "formik";
 import ErrorMessage from "./errormessage";
 
-const DrinkField=({label,name,as="text",placeholder,errors,component="div"})=>{
+const DrinkField = ({label, name, as = "input", filedtype = "text", placeholder, errors, component = "div"}) => {
     return (
         <div className="m-b-10">
             <label htmlFor={name}>{label}</label>
-            <Field className="orderDetail__item" as={as} name={name} id={name} placeholder={placeholder}/>
+            <Field className="orderDetail__item" as={as} type={filedtype} name={name} id={name}
+                   placeholder={placeholder}/>
             <ErrorMessage errors={errors} component={component} className="error" name={name}/>
         </div>
-)
+    )
 };
 
-DrinkField.propTypes={
-    label:PropTypes.string.isRequired,
-    name:PropTypes.string.isRequired
+DrinkField.propTypes = {
+    label: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
 
 };
 
