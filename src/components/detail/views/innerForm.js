a1706bda9079b481bbfbee83976e355b83ea6623
ko tainen <TonyK@hyvesolutions.com>
2020-03-22 18:38:35 +0800

    update inner form

--- src/components/detail/views/innerForm.js
+++ src/components/detail/views/innerForm.js
@@ -3,15 +3,15 @@ import {Form, Formik} from "formik";
 import DrinkField from "./drinkfield";
 import OrderControl from "./ordercontrol";
 
-const InnerForm=()=>(
-        <Form className="dailyDrink__orderList">
-            <DrinkField label="Name:" name="name" placeholder="請輸入飲料"/>
-            <DrinkField label="Price:" name="price" filedtype="number" placeholder="請輸入價錢"/>
-            <DrinkField label="Notes:" name="notes" as="textarea" placeholder="備註：甜度、冰塊、加料…"/>
-            <OrderControl handleCancel={(e) => {
-                e.preventDefault();
-            }}/>
-        </Form>
-    );
+const InnerForm = props => (
+    <Form className="dailyDrink__orderList">
+        <DrinkField label="Name:" name="name" placeholder="請輸入飲料"/>
+        <DrinkField label="Price:" name="price" filedtype="number" placeholder="請輸入價錢"/>
+        <DrinkField label="Notes:" name="notes" as="textarea" placeholder="備註：甜度、冰塊、加料…"/>
+        <OrderControl handleCancel={(e) => {
+            e.preventDefault();
+        }}/>
+    </Form>
+);
 
 export default InnerForm;
