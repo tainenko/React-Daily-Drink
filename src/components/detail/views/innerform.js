8f55603a8b940544194a1329919d047475b6724b
ko tainen <TonyK@hyvesolutions.com>
2020-03-22 16:08:00 +0800

    Add innerForm

---
+++ src/components/detail/views/innerform.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Form, Formik} from "formik";
+import DrinkField from "./drinkfield";
+import OrderControl from "./ordercontrol";
+
+const innerForm=()=>(
+        <Form className="dailyDrink__orderList">
+            <DrinkField label="Name:" name="name" placeholder="請輸入飲料"/>
+            <DrinkField label="Price:" name="price" filedtype="number" placeholder="請輸入價錢"/>
+            <DrinkField label="Notes:" name="notes" as="textarea" placeholder="備註：甜度、冰塊、加料…"/>
+            <OrderControl handleCancel={(e) => {
+                e.preventDefault();
+            }}/>
+        </Form>
+    );
+
+export default innerForm;
