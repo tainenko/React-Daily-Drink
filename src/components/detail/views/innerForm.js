b42a43802e9d86ae0f1e3ec7533fa7f588598eaa
ko tainen <TonyK@hyvesolutions.com>
2020-03-23 00:51:23 +0800

    add type in InnerForm

--- src/components/detail/views/innerForm.js
+++ src/components/detail/views/innerForm.js
@@ -5,9 +5,9 @@ import OrderControl from "./ordercontrol";
 
 const InnerForm = props => (
     <Form className="dailyDrink__orderList">
-        <DrinkField label="Name:" name="name" placeholder="請輸入飲料"/>
-        <DrinkField label="Price:" name="price" filedtype="number" placeholder="請輸入價錢"/>
-        <DrinkField label="Notes:" name="notes" as="textarea" placeholder="備註：甜度、冰塊、加料…"/>
+        <DrinkField label="Name:" name="name" type="text"  placeholder="請輸入飲料"/>
+        <DrinkField label="Price:" name="price" type="number" placeholder="請輸入價錢"/>
+        <DrinkField label="Notes:" name="notes" as="textarea" type="text"  placeholder="備註：甜度、冰塊、加料…"/>
         <OrderControl handleCancel={(e) => {
             e.preventDefault();
         }}/>
