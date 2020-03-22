c45b15566ccb5fd053d951686e37e7967d2abb54
ko tainen <TonyK@hyvesolutions.com>
2020-03-22 11:24:24 +0800

    add as prop in item

--- src/components/detail/views/item.js
+++ src/components/detail/views/item.js
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 import {Field} from "formik";
 import ErrorMessage from "./errormessage";
 
-const DetailItem=({label,name,placeholder,errors,component="div"})=>{
+const DetailItem=({label,name,as="text",placeholder,errors,component="div"})=>{
     return (
         <div className="m-b-10">
             <label htmlFor={name}>{label}</label>
-            <Field className="orderDetail__item" name={name} id={name} placeholder={placeholder}/>
+            <Field className="orderDetail__item" as={as} name={name} id={name} placeholder={placeholder}/>
             <ErrorMessage errors={errors} component={component} className="error" name={name}/>
         </div>
 )
