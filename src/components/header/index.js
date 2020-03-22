02cb5cbbd388ff5c052a51ba809cf1d35503e31f
ko tainen <TonyK@hyvesolutions.com>
2020-03-22 12:48:57 +0800

    fix syntax error

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import Proptypes from "prop-types";
 
 const Header = ({title}) => (
     <div className="dailyDrink__title">
-        <p className="title p-20">title</p>
+        <p className="title p-20">{title}</p>
     </div>
 );
 Header.proptypes = {
