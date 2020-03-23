87a02f0fcb2ab8e6ecf438b796289372019050aa
ko tainen <TonyK@hyvesolutions.com>
2020-03-23 20:04:03 +0800

    add actions in index of drinkdetail

--- src/components/drinkdetail/index.js
+++ src/components/drinkdetail/index.js
@@ -1,5 +1,6 @@
 import reducer from "./reducer";
+import * as actions from "./actions";
 import OrderDetailPage from "./views/detail"
 
 
-export {OrderDetailPage,reducer};
+export {OrderDetailPage,reducer,actions};
