73dbedfbc1576a1d5821cd0b55c1633260e38365
ko tainen <TonyK@hyvesolutions.com>
2020-03-23 15:54:36 +0800

    remove test item from index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import Provider from "react-redux";
+import {Provider} from "react-redux";
 import store from "./components/store";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./components/common/style.scss";
+import "./components/common/reset.scss";
+import DrinkListPage from "./components/orderlist/views/drinklist";
+import {OrderDetailPage} from "./components/detail"
+
 const wrapper = document.getElementById("container");
 wrapper ? ReactDOM.render(
     <Provider store={store}>
-
+        <DrinkListPage/>
+        <OrderDetailPage/>
     </Provider>
-    , wrapper) :false;
+    , wrapper) : false;
