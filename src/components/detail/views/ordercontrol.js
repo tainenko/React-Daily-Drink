50d8310e43dfca122a24dbefd7b180ad176766eb
ko tainen <TonyK@hyvesolutions.com>
2020-03-23 10:36:47 +0800

    add PropTypes of OrderControl

--- src/components/detail/views/ordercontrol.js
+++ src/components/detail/views/ordercontrol.js
@@ -1,10 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const OrderControl=({isSubmitClickable,handleSubmit,handleCancel})=>(
+const OrderControl = ({isSubmitClickable, handleSubmit, handleCancel}) => (
     <div className="dailyDrink__orderList__control">
-        <button type="submit" className={`btn btn-add transition ${isSubmitClickable?"":"disable"}`} onClick={handleSubmit}>Submit</button>
+        <button type="submit" className={`btn btn-add transition ${isSubmitClickable ? "" : "disable"}`}
+                onClick={handleSubmit}>Submit
+        </button>
         <button type="input" className="btn btn-delete transition" onClick={handleCancel}>Cancel</button>
     </div>
 );
+
+OrderControl.propTypes = {
+    isSubmitClickable: PropTypes.bool.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+    handleCancel: PropTypes.func.isRequired
+};
+
 
 export default OrderControl;
