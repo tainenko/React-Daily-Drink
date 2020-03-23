1d53210000bbcf6e12dad790cb9a09aabb5bd8d4
ko tainen <TonyK@hyvesolutions.com>
2020-03-23 10:34:37 +0800

    add isSubmitClickable

--- src/components/detail/views/ordercontrol.js
+++ src/components/detail/views/ordercontrol.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const OrderControl=({handleSubmit,handleCancel})=>(
+const OrderControl=({isSubmitClickable,handleSubmit,handleCancel})=>(
     <div className="dailyDrink__orderList__control">
-        <button type="submit" className="btn btn-add transition" onClick={handleSubmit}>Submit</button>
+        <button type="submit" className={`btn btn-add transition ${isSubmitClickable?"":"disable"}`} onClick={handleSubmit}>Submit</button>
         <button type="input" className="btn btn-delete transition" onClick={handleCancel}>Cancel</button>
     </div>
 );
