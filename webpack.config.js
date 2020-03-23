c0d9c58ff41029b80ac5dc6e06b9ea06e35294c5
ko tainen <TonyK@hyvesolutions.com>
2020-03-23 18:57:14 +0800

    fix error of cannot GET *url*

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/'
+    },
     module: {
         rules: [
             {
@@ -43,6 +50,9 @@ module.exports = {
                 ]
             }
         ]
+    },
+    devServer: {
+        historyApiFallback: true,
     },
     plugins: [
         new HtmlWebPackPlugin({
