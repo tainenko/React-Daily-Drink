6c6b40459917211a02362abfa0881d63e9536df2
ko tainen <TonyK@hyvesolutions.com>
2020-03-23 20:25:17 +0800

    remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import {Provider} from "react-redux";
-
-import {Router,Route} from 'react-router-dom';
+import {Router, Route} from 'react-router-dom';
 import {createBrowserHistory} from "history";
 import store from "./components/store";
-import DrinkListPage from "./components/drinklist/views/drink-list-page";
+import DrinkListPage from "./components/drinklist";
 import {DrinkDetailPage} from "./components/drinkdetail"
 
 const history = createBrowserHistory();
 
-export default class App extends React.Component{
-    render(){
-        return(
+export default class App extends React.Component {
+    render() {
+        return (
             <Provider store={store}>
                 <Router history={history}>
-                        <Route exact path="/" component={DrinkListPage} />
-                        <Route path="/detail" component = {DrinkDetailPage}/>
+                    <Route exact path="/" component={DrinkListPage}/>
+                    <Route path="/detail" component={DrinkDetailPage}/>
                 </Router>
             </Provider>
 
