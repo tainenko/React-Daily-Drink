eb1d6858971e167166c14f49b819a2559483b6ed
ko tainen <TonyK@hyvesolutions.com>
2020-03-23 20:15:10 +0800

    update error message

--- src/components/drinkdetail/views/drink-detail-error-schema.js
+++ src/components/drinkdetail/views/drink-detail-error-schema.js
@@ -3,14 +3,14 @@ import * as Yup from "yup";
 
 const DrinkDetailErrorSchema = Yup.object().shape({
     name: Yup.string()
-        .min(2, 'Too Short!')
-        .max(70, 'Too Long!')
-        .required('Required'),
+        .min(2, '必須大於兩個字')
+        .max(70, '不可超過70字')
+        .required('必填項目'),
     price: Yup.number()
         .lessThan(200,"價格必須小於200元")
         .moreThan(0,"價格必須大於零")
         .integer("價格不能為小數")
-        .required('Required'),
+        .required('必填項目'),
     notes:Yup.string()
         .max(300,'備註不可超過300字')
 });
