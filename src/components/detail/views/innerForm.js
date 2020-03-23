5fbd841f2ce97d93c24c361c435d5ab32edc2e8d
ko tainen <TonyK@hyvesolutions.com>
2020-03-23 10:42:08 +0800

    update InnerForm

--- src/components/detail/views/innerForm.js
+++ src/components/detail/views/innerForm.js
@@ -1,17 +1,19 @@
 import React from "react";
-import {Form, Formik} from "formik";
+import {Form, Formik, useField} from "formik";
 import DrinkField from "./drinkfield";
 import OrderControl from "./ordercontrol";
 
-const InnerForm = props => (
-    <Form className="dailyDrink__orderList">
-        <DrinkField label="Name:" name="name" type="text"  placeholder="請輸入飲料"/>
-        <DrinkField label="Price:" name="price" type="number" placeholder="請輸入價錢"/>
-        <DrinkField label="Notes:" name="notes" as="textarea" type="text"  placeholder="備註：甜度、冰塊、加料…"/>
-        <OrderControl handleCancel={(e) => {
-            e.preventDefault();
-        }}/>
-    </Form>
-);
+const InnerForm = props => {
+    const [field, meta, helpers] = useField(props);
+    return (
+        <Form className="dailyDrink__orderList">
+            <DrinkField label="Name:" name="name" type="text" placeholder="請輸入飲料"/>
+            <DrinkField label="Price:" name="price" type="number" placeholder="請輸入價錢"/>
+            <DrinkField label="Notes:" name="notes" as="textarea" type="text" placeholder="備註：甜度、冰塊、加料…"/>
+            <OrderControl isSubmitClickable={!!(field.name && field.price)} handleSubmit={props.handleSubmit}
+                          handleCancel={props.handleCancel}/>
+        </Form>
+    )
+};
 
 export default InnerForm;
