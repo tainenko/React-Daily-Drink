023dc32fd80cc348b9710339ca0fde173baacc5e
ko tainen <TonyK@hyvesolutions.com>
2020-04-06 10:57:26 +0800

    Move store to store folder

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,5 @@
 import {combineReducers} from "redux";
-import {reducer as OrderReducer} from "../drinkdetail"
+import {reducer as OrderReducer} from "../components/drinkdetail"
 const reducers=combineReducers({
     orders:OrderReducer
 });
