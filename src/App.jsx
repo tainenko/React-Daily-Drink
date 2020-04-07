e61930dc6ac5dff26d9193779f1fcde4345f2399
ko tainen <TonyK@hyvesolutions.com>
2020-04-07 18:08:36 +0800

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {Provider} from "react-redux";
 import {Router, Route} from 'react-router-dom';
 import {createBrowserHistory} from "history";
-import store from "./components/store";
-import DrinkListPage from "./components/drinklist";
-import {DrinkDetailPage} from "./components/drinkdetail"
+import store from "./store";
+import DrinkListPage from "./components/drink/list";
+import {DrinkDetailPage} from "./components/drink/detail"
 
 const history = createBrowserHistory();
 
