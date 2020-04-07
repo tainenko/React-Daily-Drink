d23abceeef1780e8481a9116458790ee756d68ff
ko tainen <TonyK@hyvesolutions.com>
2020-04-07 17:31:30 +0800

    add testId in list control

--- src/components/drink/list/views/Control.js
+++ src/components/drink/list/views/Control.js
@@ -3,12 +3,14 @@ import React from "react";
 const Control = ({isDelClickable, handleAddItem, handleDeleteBtn}) => (
     <div className="dailyDrink__list__control">
         <button
+            data-testid="add button"
             className="btn btn-add transition"
             onClick={handleAddItem}
         >
             + Add
         </button>
         <button
+            data-testid="del button"
             className={`btn btn-delete transition ${isDelClickable ? "" : "disable"}`}
             onClick={handleDeleteBtn}
         >
